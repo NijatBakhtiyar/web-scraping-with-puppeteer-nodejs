c942a10cf930ea68a83016414502083b2d33a8cf
nijatbakhtiyar@gmail.com <NijatBakhtiyar>
2022-10-19 00:58:35 +0400

    taking content by element selector

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
 
 async function start() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://en.wikipedia.org/wiki/JavaScript");
-  await page.screenshot({ path: "javascript.png", fullPage: true });
+  await page.goto("https://learnwebcode.github.io/practice-requests/");
 
+  const names = await page.evaluate(() => {
+    const content = Array.from(document.querySelectorAll(".info strong"));
+    return content.map((strong) => strong.textContent);
+  });
+
+  await fs.writeFile("names.txt", names.join("\n"));
   await browser.close();
 }
 
