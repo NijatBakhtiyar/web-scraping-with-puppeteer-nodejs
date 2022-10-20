7545a46873c149c96c9d6b24a027b0c77c9bde95
nijatbakhtiyar@gmail.com <NijatBakhtiyar>
2022-10-21 02:28:43 +0400

    page navigation && form fill

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
+const cron = require("node-cron");
 
 async function start() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://learnwebcode.github.io/practice-requests/");
 
-  //   const names = await page.evaluate(() => {
-  //     const content = Array.from(document.querySelectorAll(".info strong"));
-  //     return content.map((strong) => strong.textContent);
-  //   });
-  //     await fs.writeFile("names.txt", names.join("\n"));
+  const [button] = await page.$x("//button[contains(., 'Click me')]");
+  await button.click();
+  await page.content("Dogs bark and cats meow.");
 
-  const photos = await page.$$eval("img", (imgs) => {
-    return imgs.map((img) => img.src);
-  });
+  const input = await page.$("input[type=text]");
+  await input.type("blue");
+  const submitBtn = await page.$x("//button[contains(., 'Submit')]");
+  submitBtn[0].click();
 
-  for (const photo of photos) {
-    const imagePage = await page.goto(photo);
-    await fs.writeFile(photo.split("/").pop(), await imagePage.buffer());
+  await page.waitForNavigation();
+  const url = await page.evaluate(() => location.pathname);
+
+  console.log(url);
+  if (url.includes("/message.html")) {
+    console.log("success");
+  } else {
+    console.log("failed");
   }
-
-  //   await browser.close();
 }
-
 start();
+cron.schedule("*/10 * * * * *", async () => {
+  start();
+});
